6eb5aa3934d034a64eea5fd1768fe7e1d995495b
Neil Pulcyn <neilpulcyn@hotmail.co.uk>
2021-11-10 15:40:34 +0000

    first commit

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,37 @@
+class RestaurantsController < ApplicationController
+  # GET /restaurants - displays the restaurants
+  before_action :set_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET/restaurants/1
+  def show; end
+
+  def new
+    # we create a new instance of restaurant via a form which was coded on the HTML.erb file
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    # we create the restaurant using strong params as a parameter to stop sneaky injection/hacking
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new # throws error message?
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+5.times do
+  restaurant = Restaurant.create!( # we use ! on create so that it throws an error message if there's an issue
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  5.times do
+    review = restaurant.reviews.create!(
+      content: Faker::Food.description,
+      rating: (0..5).to_a.sample
+    )
+  end
+  puts "Creating restaurant with id #{restaurant.id}"
+end
+
+puts 'Finished!'
